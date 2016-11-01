304c493a887aea492610aa0f9796f5ac51027297
Marcelo Damasceno de Melo <marcelodamasceno@gmail.com>
2016-10-31 23:16:53 -0300

    Adds maps

--- js/helper.js
+++ js/helper.js
@@ -3,11 +3,7 @@
 This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
 
 Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
+ Cameron Pittman */
 /*
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
@@ -64,8 +60,8 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
+    var iName = inName() || function() {};
+    $('#name').html(iName);
   });
 });
 
@@ -74,13 +70,11 @@ The next few lines about clicks are for the Collecting Click Locations quiz in L
 */
 clickLocations = [];
 
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
+function logClicks(x, y) {
+  clickLocations.push({
+    x: x,
+    y: y
+  });
   console.log('x location: ' + x + '; y location: ' + y);
 }
 
@@ -95,7 +89,7 @@ This is the fun part. Here's where we generate the custom Google Map for the web
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
-var map;    // declares a global map variable
+var map; // declares a global map variable
 
 
 /*
@@ -113,7 +107,7 @@ function initializeMap() {
   For the map to be displayed, the googleMap var must be
   appended to #mapDiv in resumeBuilder.js. 
   */
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+  map = new google.maps.Map(document.querySelector('#mapDiv'), mapOptions);
 
 
   /*
@@ -132,7 +126,8 @@ function initializeMap() {
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide: 
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
+    // 
+    education.schools.forEach(function(school) {
       locations.push(school.location);
     });
 
@@ -140,7 +135,7 @@ function initializeMap() {
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide: 
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
+    work.jobs.forEach(function(job) {
       locations.push(job.location);
     });
 
@@ -155,10 +150,10 @@ function initializeMap() {
   function createMapMarker(placeData) {
 
     // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
+    var lat = placeData.geometry.location.lat(); // latitude from the place service
+    var lon = placeData.geometry.location.lng(); // longitude from the place service
+    var name = placeData.formatted_address; // name of the place from the place service
+    var bounds = window.mapBounds; // current boundaries of the map window
 
     // marker is an object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
@@ -195,6 +190,7 @@ function initializeMap() {
   function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       createMapMarker(results[0]);
+
     }
   }
 
@@ -209,15 +205,16 @@ function initializeMap() {
     var service = new google.maps.places.PlacesService(map);
 
     // Iterates through the array of locations, creates a search object for each location
-      locations.forEach(function(place){
+    locations.forEach(function(place) {
       // the search request object
       var request = {
         query: place
       };
-
       // Actually searches the Google Maps API for location data and runs the callback
       // function with the search results after each search.
+      if(request.query!=null){
       service.textSearch(request, callback);
+      }
     });
   }
 
@@ -231,6 +228,7 @@ function initializeMap() {
   // the locations array
   pinPoster(locations);
 
+
 }
 
 /*
@@ -238,11 +236,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+//Make sure the map bounds get updated on page resize
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,91 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio={
+  	"name":"Marcelo Damasceno\n",
+  	"role":"Lecturer\n",
+  	"contact":"marcelodamasceno@gmail.com\n",
+  	"pictureUrl":"images/me.jpg\n",
+  	"welcome":"What's Up?\n",
+  	"skills":["Infinite\n"],
+    "contacts" : {
+    "mobile" : "555-55-55",
+    "skype" : "minkova.tanya", 
+    "location" : "Parnamirim, RN",
+    "email" : "t.minkova@live.com"
+  }
+  }
+var work={
+  "jobs":[{
+  	"title":"Lecturer",
+  	"employer":"IFRN",
+  	"years":6,
+  	"location":"São Gonçalo do Amarante,RN",
+    "description":"bla bla bla bla bla"}]
+  }
+var education={
+  "schools":[{
+  	"name":"UFRN",
+  	"years":4,
+  	"location":"UFRN,Natal"}]
+  }
+
+  var projects={
+    "projects":[{
+      "title":"teste",
+      "dates":"2016",
+      "description":"bla-bla-bla"
+    }]
+  }
+  
+  var formmatedRole=HTMLheaderRole.replace("%data%",bio.role)
+  $("#header").prepend(formmatedRole)
+
+  var formmatedName=HTMLheaderName.replace("%data%",bio.name)
+  $("#header").prepend(formmatedName)
+
+
+  $("#main").append(work.position)
+
+  $("#education").append(HTMLschoolStart)
+
+for(school in education.schools){
+   var formattedName=HTMLschoolName.replace("%data%",education.schools[school].name)
+  $(".education-entry:last").append(education.schools[0].name)
+}
+ 
+  //
+  
+  function inName(){
+  	var newName=bio.name.trim().split(" ");
+  	var cap=newName[1].toUpperCase();
+  	newName[1]=cap;
+  	return newName[0]+" "+newName[1];
+  }
+
+$("#main").append(internationalizeButton);
+
+projects.display=function(){
+   for(project in projects.projects){
+    $("#projects").append(HTMLprojectStart);
+
+    var formmatedTitle=HTMLprojectTitle.replace("%data%",projects.projects[project].title);
+    $(".project-entry:last").append(formmatedTitle);
+
+    var formmatedDates=HTMLprojectDates.replace("%data%",projects.projects[project].dates);
+    $(".project-entry:last").append(formmatedDates);
+
+    var formmatedDescription=HTMLprojectDescription.replace("%data%",projects.projects[project].description);
+    $(".project-entry:last").append(formmatedDescription);
+
+    if(projects.projects[project].images>0){
+      for(image in projects.projects[project].images){
+        var formattedImages=HTMLprojectImage.replace("%data%",projects.projects[project].images[image]);
+        $(".project-entry:last").append(formmatedImages);        
+      }
+
+    }
+  }
+  
+}
+
+projects.display();
+
+$("#mapdiv").append(googleMap);
